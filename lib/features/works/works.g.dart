f99b6718847f4378859bf58a323b252155ce7c8b
K9i-0 <geukfyger@gmail.com>
2023-03-08 21:25:55 +0900

    ✨riverpodバージョン最新

--- lib/features/works/works.g.dart
+++ lib/features/works/works.g.dart
@@ -6,6 +6,20 @@ part of 'works.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+String _$worksHash() => r'e4efe9c5f628f6531a96df2ef868436cab8f82aa';
+
+/// See also [works].
+@ProviderFor(works)
+final worksProvider = AutoDisposeProvider<List<Work>>.internal(
+  works,
+  name: r'worksProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$worksHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef WorksRef = AutoDisposeProviderRef<List<Work>>;
 String _$worksSortOrderHash() => r'53286dbcdef3b32e8c7df4b8ccf12a04ca94edc5';
 
 /// See also [WorksSortOrder].
@@ -22,18 +36,4 @@ final worksSortOrderProvider =
 );
 
 typedef _$WorksSortOrder = AutoDisposeNotifier<WorksSortOrderState>;
-String _$worksHash() => r'e4efe9c5f628f6531a96df2ef868436cab8f82aa';
-
-/// See also [works].
-@ProviderFor(works)
-final worksProvider = AutoDisposeProvider<List<Work>>.internal(
-  works,
-  name: r'worksProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product') ? null : _$worksHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-typedef WorksRef = AutoDisposeProviderRef<List<Work>>;
 // ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
