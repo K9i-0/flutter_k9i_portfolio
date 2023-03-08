74352cd9e59578fd623b07effa0f4e2deae4e647
K9i-0 <geukfyger@gmail.com>
2023-03-08 21:26:07 +0900

    ♻️grinder

--- tool/grind.dart
+++ tool/grind.dart
@@ -17,23 +17,62 @@ Future<void> _runCommand({
   stderr.addStream(process.stderr);
 }
 
+@Task('build alias')
+void b() => build();
+
 @Task('flutter pub run build_runner build --delete-conflicting-outputs')
-void b() {
+void build() {
   _runCommand(command: 'flutter pub run build_runner build -d');
 }
 
+@Task('watch alias')
+void w() => watch();
+
 @Task('flutter pub run build_runner watch --delete-conflicting-outputs')
-void w() {
+void watch() {
   _runCommand(command: 'flutter pub run build_runner watch -d');
 }
 
+@Task('l10n alias')
+void l() => l10n();
+
 @Task('flutter gen-l10n')
-void l() {
+void l10n() {
   _runCommand(command: 'flutter gen-l10n');
 }
 
+@Task('server alias')
+void s() => server();
+
+// ローカルネットワークでアクセスできるようにする
+// https://zenn.dev/toridori/articles/673cfe00628bb4
+@Task('flutter run -d web-server --web-hostname=0.0.0.0 --web-port=50505')
+void server() {
+  final result = Process.runSync('ifconfig', [], runInShell: true);
+  if (result.exitCode != 0) {
+    throw Exception(
+        'ifconfig command failed with exit code ${result.exitCode}');
+  }
+  final output = result.stdout.toString();
+  final lines = output.split('\n');
+  for (var line in lines) {
+    if (line.contains('inet ') && !line.contains('127.0.0.1')) {
+      final ip = line.split(' ')[1];
+      log(ip);
+      _runCommand(
+        command:
+            'flutter run -d web-server --web-hostname=$ip --web-port=50505',
+      );
+      break;
+    }
+  }
+}
+
+@Task('updatePods alias')
+void u() => updatePods();
+
 @Task('CocoaPodsのアップデート(パッケージアプデ時に使う)')
-void up() {
+void updatePods() {
   run(
     'rm',
     arguments: ['-rf', 'Pods/'],
