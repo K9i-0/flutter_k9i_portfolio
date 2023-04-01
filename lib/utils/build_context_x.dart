7d1e67a91740235f9c0141383361f9752e5a41e4
K9i-0 <geukfyger@gmail.com>
2023-04-01 22:38:30 +0900

    ✨l10n.yamlを変更

--- lib/utils/build_context_x.dart
+++ lib/utils/build_context_x.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_k9i_portfolio/resources/l10n/generated/l10n.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 extension BuildContextX on BuildContext {
   TextTheme get textTheme => Theme.of(this).textTheme;
