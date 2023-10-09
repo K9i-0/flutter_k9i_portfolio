08301080d6d9f7e20bdf1c72759e1d8760b83902
K9i-0 <geukfyger@gmail.com>
2023-10-09 14:02:03 +0900

    improve: lintを変えた

--- lib/common/shared_preferences.g.dart
+++ lib/common/shared_preferences.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: type=lint, duplicate_ignore
+
 part of 'shared_preferences.dart';
 
 // **************************************************************************
@@ -22,4 +24,5 @@ final sharedPreferencesProvider =
 );
 
 typedef SharedPreferencesRef = AutoDisposeProviderRef<SharedPreferences>;
-// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
--- tool/grind.dart
+++ tool/grind.dart
@@ -1,12 +1,10 @@
 import 'dart:io';
 
-import 'package:dart_openai/openai.dart';
-import 'package:dotenv/dotenv.dart';
 import 'package:grinder/grinder.dart';
 import 'package:k9i_cli/k9i_cli.dart';
 
 void main(List<String> args) {
-  addAllTasks(enableAlias: true);
+  addAllTasks(enableAlias: true, useFvm: true);
   grind(args);
 }
 
@@ -22,58 +20,4 @@ Future<void> _runCommand({
   );
   await stdout.addStream(process.stdout);
   await stderr.addStream(process.stderr);
-}
-
-@Task('commitMessage alias')
-void c() => commitMessage();
-
-@Task('コミットメッセージを生成する')
-Future<void> commitMessage() async {
-  final diffCommandResult = Process.runSync('git', ['diff'], runInShell: true);
-  if (diffCommandResult.exitCode != 0) {
-    throw Exception(
-      'git diff command failed with exit code ${diffCommandResult.exitCode}',
-    );
-  }
-  final diff = diffCommandResult.stdout.toString();
-  log(diff);
-
-  // .envファイルを読み込む
-  final env = DotEnv()..load();
-  final apiKey = env['OPEN_AI_API_KEY']!;
-  OpenAI.apiKey = apiKey;
-  final chatGptResult = await OpenAI.instance.chat.create(
-    model: 'gpt-3.5-turbo',
-    messages: [
-      OpenAIChatCompletionChoiceMessageModel(
-        content: '''
-あなたはgitのコミットメッセージを考えるアシスタントです。gitの差分を入力されたら、そこからコミットメッセージを考えてください。
-
-考慮事項
-- 日本語でお願いします
-- 返答はコミットメッセージの内容だけを返してください
-- +ではじまる行が追加された行、-ではじまる行が削除された行です
-
-例
-- 機能追加：ユーザー登録画面の実装
-- 修正：検索結果のページネーションの不具合を解消
-- リファクタリング：無駄なコードの削除と整理
-- テスト追加:新規APIエンドポイントのテストケースを作成
-- ドキュメント更新:README.mdに使用方法を追記
-''',
-        role: OpenAIChatMessageRole.system,
-      ),
-      OpenAIChatCompletionChoiceMessageModel(
-        content: diff,
-        role: OpenAIChatMessageRole.user,
-      ),
-    ],
-  );
-  final commitMessage = chatGptResult.choices.first.message.content;
-  log(commitMessage);
-  await Process.run(
-    'zsh',
-    ['-c', 'echo $commitMessage | pbcopy'],
-    runInShell: true,
-  );
 }
